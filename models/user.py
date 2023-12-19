3237a9a610084ff5544642f20d9648ee98890ff3
Rachid KABBAB <r.kabbab@gmail.com>
2023-12-19 14:58:49 -0800

    removed if else

--- models/user.py
+++ models/user.py
@@ -8,15 +8,15 @@ from models import storage_type
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
-    if (storage_type == "db"):
-        __tablename__ = "users"
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        """places = relationship("Place", backref="user")"""
-    else:
+    """if (storage_type == "db"):"""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="user")
+    """else:
         email = ''
         password = ''
         first_name = ''
-        last_name = ''
+        last_name = '' """

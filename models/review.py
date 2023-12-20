955d30a48e8df0d14c99eada90bcf550f5d0ab81
Rachid KABBAB <r.kabbab@gmail.com>
2023-12-20 10:48:04 -0800

    fixed foreing keys issue

--- models/review.py
+++ models/review.py
@@ -10,8 +10,8 @@ class Review(BaseModel, Base):
     """if storage_type == "db":"""
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     """
     else:
         place_id = ""

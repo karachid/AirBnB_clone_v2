219eb10650e8deebb85f73439c71632b38d230ea
Rachid KABBAB <r.kabbab@gmail.com>
2024-01-28 11:44:25 +0000

    7 improving engines

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -87,3 +87,7 @@ class DBStorage:
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """Calling remove() method on the private session attribute"""
+        self.__session.remove()
